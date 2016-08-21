f788c1293777e20fdcd71998b498fcfa6c0a7869
Michael Durkin <durkinm2@wit.edu>
2016-08-21 08:31:47 -0700

    installed some dependencies

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var sass = require('node-sass');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -55,6 +59,13 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+//sass.render({
+//  file: "/sass/home.scss",
+  //[, options..]
+//}, function(err, result) { /*...*/ });
+// OR
+
 
 
 module.exports = app;
